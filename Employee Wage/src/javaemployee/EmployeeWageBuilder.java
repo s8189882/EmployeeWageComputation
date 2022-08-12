94a121945511990f5741affd09b2f91ebf28dd52
s8189882 <s8189882@gmail.com>
2022-08-12 12:00:12 +0530

    Introduced Part time employee work hours and wages

--- Employee Wage/src/javaemployee/EmployeeWageBuilder.java
+++ Employee Wage/src/javaemployee/EmployeeWageBuilder.java
@@ -4,6 +4,12 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Employee Wage Computation Program!");
+		//Add Part time Employee & Wage 
+		//Assume Part time Hour is 8
+		//wages per hour is 20
+		int workingHours = 8;
+		int dailyWage = workingHours * 20;
+		System.out.println("Daily Part Time Wage is : $" + dailyWage);
+		
 	}
 }
