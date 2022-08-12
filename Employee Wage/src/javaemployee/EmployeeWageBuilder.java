a5f5f1b08f413394aaff706329030a98275a4fe7
s8189882 <s8189882@gmail.com>
2022-08-12 12:30:28 +0530

    Solving using switch case statement

--- Employee Wage/src/javaemployee/EmployeeWageBuilder.java
+++ Employee Wage/src/javaemployee/EmployeeWageBuilder.java
@@ -4,6 +4,39 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Employee Wage Computation Program!");
+		double empCheck = Math.floor(Math.random()*10) % 2;
+		int workingHours = 0;
+		int dailyWage = 0;
+		int empRatePerHr = 20;
+		int isFullTime = 0;
+		int isPartTime = 0;
+
+		
+		
+		//Solving using switch case
+		switch ((int)(empCheck)) {
+		
+		case 1 :
+			isFullTime = 1;
+			workingHours = 8;
+			System.out.println("Employee is working Full Time");
+			System.out.println("Working hours of Employee are : " + workingHours);
+			break;
+			
+		case 0 :
+			isPartTime = 0;
+			workingHours = 8;
+			System.out.println("Employee is working Part Time");
+			System.out.println("Working hours of Employee are : " + workingHours);
+		
+			break;
+			
+		default :
+			workingHours = 0;
+			break;
+		
+		}
+		
+		
 	}
 }
