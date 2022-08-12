1aa4030656385d79faa057ae86507fae36507f3d
s8189882 <s8189882@gmail.com>
2022-08-12 13:08:26 +0530

    Displaying Welcome to Employee Wage Computation Program on Master Branch

--- Employee Wage/src/javaemployee/EmployeeWageBuilder.java
+++ Employee Wage/src/javaemployee/EmployeeWageBuilder.java
@@ -4,6 +4,8 @@ public class EmployeeWageBuilder {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Employee Wage Computation Program!");
-	}
+		//Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+ 		System.out.println("Welcome to Employee Wage Computation Program!");
+ 		
+ 	}
 }
